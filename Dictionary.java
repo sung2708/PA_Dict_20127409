fbf5dca6741e6bafc81158537e0ddf1449267e4c
sungp2708 <aki18082002@gmail.com>
2022-11-27 08:08:18 +0700

    updated class dictionary, many meanings

--- Dictionary.java
+++ Dictionary.java
@@ -1,16 +1,25 @@
+import java.util.*;
+
 public class Dictionary{
     private String _Slang;
-    private String _Mean;
+    private Vector<String> _Mean = new Vector<String>();
 
     public String getSlang(){return this._Slang;}
-    public String getMean(){return this._Mean;}
+    public Vector<String> getMean(){return this._Mean;}
 
     public void setSlang(String Slang){this._Slang = Slang;}
-    public void setMean(String Mean){this._Mean = Mean;}
+    public void setMean(Vector<String> Mean){this._Mean = Mean;}
 
     public Dictionary createSlang(Dictionary d, String []tmp){
+        Vector<String>mean = new Vector<String>();
+        String str = tmp[1];
+        str = str.replaceAll("\\s+","");
+        String[] split = str.trim().split("[|]");
         d.setSlang(tmp[0]);
-        d.setMean(tmp[1]);
+        for(String s:split){
+            mean.add(s);
+        }
+        d.setMean(mean);
         return d;
     }
 }
